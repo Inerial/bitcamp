5727e8ac9ed13164db791fa46ae2dfb4abe0eea3
Inerial <rlxo132@naver.com>
2020-05-12 17:34:36 +0900

    1

--- keras/keras02_1.py
+++ keras/keras02_1.py
@@ -3,8 +3,8 @@ from keras.models import Sequential, Model
 from keras.layers import Dense,Dropout,Activation,Embedding,LSTM
 import numpy as np
 
-x_train = np.array([-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7])
-y_train = np.array([50, 58, 66, 74, 82, 90, 98, 106, 114, 122,130,138,146,154,162])
+x_train = np.array([-2,-1,0,1,2,3,4,5,6,7])
+y_train = np.array([90, 98, 106, 114, 122,130,138,146,154,162])
 x_test = np.array([101,102,103,104,105,106,107,108,109,110])
 y_test = np.array([914,922,930,938,946,954,962,970,978,986])
 
@@ -18,7 +18,7 @@ model.summary()
 
 model.compile(loss='mse', optimizer='adam',metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=600, batch_size=1)##, validation_data = (x_test, y_test))
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_data = (x_test, y_test))
 loss,acc = model.evaluate(x_test ,y_test ,batch_size=1)
 
 print("loss : ", loss)
