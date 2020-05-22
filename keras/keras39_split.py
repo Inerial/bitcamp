c686d4b36f9a50a22e05fa4ff9fcbbdb32b1efdf
Inerial <rlxo132@naver.com>
2020-05-22 17:25:22 +0900

    5

--- keras/keras39_split.py
+++ keras/keras39_split.py
@@ -4,29 +4,16 @@ from keras.layers import Dense, LSTM
 
 #1.데이터
 size = 5
-a = np.array(range(1,11))
-b = np.array([range(1,11), range(2,12)])
-
+a = np.array(range(11))
 
 def split_x(seq, size):
-    if len(seq.shape) == 1:
-        aaa = []
-        for i in range(len(seq) - size + 1):
-            subset = seq[i:i+size]
-            aaa.append(subset)
-        print(type(aaa))
-        aaa = np.array(aaa)
-        return aaa.reshape(aaa.shape[0], aaa.shape[1], 1)
-    elif len(seq.shape) == 2:
-        aaa = []
-        for i in range(len(seq.T) - size + 1):
-            subset = seq.T[i:i+size]
-            aaa.append(subset)
-        print(type(aaa))
-        return np.array(aaa)
-    else :
-        print("error")
-        return
+    aaa = []
+    for i in range(len(seq) - size + 1):
+        subset = seq[i:i+size]
+        aaa.append(subset)
+    print(type(aaa))
+    return np.array(aaa)
+
 
 '''
 seq = 나눌 배열, size = time_step
@@ -40,9 +27,5 @@ i ~ i+time_step 까지의 배열요소를 subset에 저장하여 aaa에 입력
 
 
 datasetA = split_x(a, size)
-datasetB = split_x(b, size)
 print("=========================")
 print(datasetA)
-print(datasetA.shape)
-print(datasetB)
-print(datasetB.shape)
--- keras/keras39_split_my.py
+++ keras/keras39_split_my.py
@@ -4,18 +4,33 @@ from keras.layers import Dense, LSTM
 
 #1.데이터
 size = 5
-a = np.array(range(11))
+a = np.array(range(1,11))
+b = np.array([range(1,11), range(2,12)])
+
 
 def split_x(seq, size):
-    aaa = []
-    for i in range(len(seq) - size + 1):
-        subset = seq[i:i+size]
-        aaa.append(subset)
-    print(type(aaa))
-    return np.array(aaa)
-
-for i in [1,3,4,2]:
-    print(i)
+    import numpy as np
+    if type(seq) != np.ndarray:
+        print("입력값이 array가 아님!")
+        return
+    elif len(seq.shape) == 1:
+        aaa = []
+        for i in range(len(seq) - size + 1):
+            subset = seq[i:i+size]
+            aaa.append(subset)
+        print(type(aaa))
+        aaa = np.array(aaa)
+        return aaa.reshape(aaa.shape[0], aaa.shape[1], 1)
+    elif len(seq.shape) == 2:
+        aaa = []
+        for i in range(len(seq.T) - size + 1):
+            subset = seq.T[i:i+size]
+            aaa.append(subset)
+        print(type(aaa))
+        return np.array(aaa)
+    else :
+        print("입력값이 3차원 이상!")
+        return
 
 '''
 seq = 나눌 배열, size = time_step
@@ -29,5 +44,9 @@ i ~ i+time_step 까지의 배열요소를 subset에 저장하여 aaa에 입력
 
 
 datasetA = split_x(a, size)
+datasetB = split_x(b, size)
 print("=========================")
 print(datasetA)
+print(datasetA.shape)
+print(datasetB)
+print(datasetB.shape)
