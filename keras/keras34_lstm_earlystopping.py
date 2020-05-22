cb6161bcb926d759395f6f39d9a4a6e25b7e0296
Inerial <rlxo132@naver.com>
2020-05-22 11:24:48 +0900

    1

---
+++ keras/keras34_lstm_earlystopping.py
@@ -0,0 +1,58 @@
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, LSTM
+
+#1. 데이터
+x1 = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[20,30,40],[30,40,50],[40,50,60]])
+x2 = np.array([[10,20,30], [20,30,40], [30,40,50], [40,50,60],[50,60,70],[60,70,80],[70,80,90],[80,90,100],[90,100,110],[100,110,120],[2,3,4],[3,4,5],[4,5,6]])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+x1_pred = np.array([55,65,75])
+x2_pred = np.array([65,75,85])
+
+#1개짜리 데이터를 넣을떄 input_dim = 1
+x1 = x1.reshape(x1.shape[0], x1.shape[1], 1)
+x2 = x2.reshape(x2.shape[0], x2.shape[1], 1)
+
+
+#2. 모델구성
+input1 = Input(shape=(3,1))
+lstm1 = LSTM(800)(input1) 
+dense1 = Dense(100)(lstm1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+dense1 = Dense(100)(dense1)
+
+
+## x2의 데이터가 x1과 전혀 어울리지 않는데다가 데이터의 개수도 적어서 weight를 0으로 만드는게 불가능에 가깝다고 보인다.
+## 따라서 85라는 적당한 데이터가 나오기 위해서는 해당 레이어를 줄여서 모델에 끼치는 영향력을 줄일 필요가 있다고 보았다.
+input2 = Input(shape=(3,1))
+lstm2 = LSTM(10)(input2) 
+dense2 = Dense(10)(lstm2)
+
+from keras.layers import concatenate
+middle1 = concatenate(inputs = [dense1, dense2])
+
+
+middle1 = Dense(1)(middle1)
+
+# 데이터의 개수인 행은 무시하고 x의 shape
+model = Model(inputs=[input1,input2], outputs=middle1)
+model.summary()
+
+#3. 실행
+model.compile(loss = 'mse', optimizer='adam')
+from keras.callbacks import EarlyStopping
+early = EarlyStopping(monitor='loss', mode = 'auto', patience=18)
+model.fit([x1,x2],y,epochs=10000,batch_size = 32, callbacks=[early])
+
+x1_pred = x1_pred.reshape(1,3,1)  ## 같은 크기의 행렬로 만들어줌
+x2_pred = x2_pred.reshape(1,3,1)  ## 같은 크기의 행렬로 만들어줌
+
+y_pred = model.predict([x1_pred, x2_pred])
+print(y_pred)
---
+++ keras/keras35_lstm_sequences.py
@@ -0,0 +1,55 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+#1. 데이터
+x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6]])
+y = np.array([4,5,6,7])
+
+print(x.shape) # (4,3)
+print(y.shape) # (4,)
+#1개짜리 데이터를 넣을떄 input_dim = 1
+x = x.reshape(4,3,1)
+#x = x.reshape(x.shape[0], x.shape[1], 1)
+
+# 4행 3열짜리 데이터를 한개씩 꺼내쓰겠다는 뜻
+
+
+## LSTM 함수는 기본적으로 2차원 함수로 출력되
+
+#2. 모델구성
+model = Sequential()
+model.add(LSTM(10, input_shape=(3,1), return_sequences=True)) 
+model.add(LSTM(10, return_sequences=False))
+# 데이터의 개수인 행은 무시하고 x의 shape
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(1))
+
+model.summary()
+## 첫 LSTM layer에 input_shape를 (3,1)을 넣었을때 return_sequences = True를 주지 않으면 리턴시키는 값의 shape가 (batch_size, time_step, feature)의 형태가 아닌
+## 기존의 (batch_size, output)의 형태로 나오게 설정되어있다.
+## 연속으로 LSTM을 쓰기 위해서는 return_sequences=True를 써주어야 한다. 반대로 Dense를 쓰기 위해서는 다시 false값을 주어야한다(디폴트값)
+
+## parameter 계산은 여전히 (input_dim + output + 1) * output * 4 이다.
+## 이떄 두번째 이후 input은 이전 LSTM의 output과 같다.
+
+## return_sequences=True값을 준다면 LSTM의 shape형태로 출력되므로 output_shape = (none, 원래 준 time_step, output크기(output노드수 == 다음레이어의 input노드수))
+
+'''
+#3. 실행
+model.compile(loss = 'mse', optimizer='adam')
+model.fit(x,y,epochs=1000,batch_size = 1)
+
+x_input = np.array([5,6,7])
+x_input = x_input.reshape(1,3,1)  ## 같은 크기의 행렬로 만들어줌
+
+y_hat = model.predict(x_input)
+print(y_hat)
+'''
