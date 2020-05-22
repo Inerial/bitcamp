acf680626b4fe10d5ef51f8f55a333e325aa006f
Inerial <rlxo132@naver.com>
2020-05-22 16:14:23 +0900

    4

--- keras/keras38_minmax.py
+++ keras/keras38_minmax.py
@@ -2,17 +2,25 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
-# 실습 : LSTM 레이어를 5개 엮어서 dense결과를 이겨내시오
 
 #1. 데이터
-x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[20,30,40],[30,40,50],[40,50,60]])
-y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
-x_pred = np.array([55,65,75])
+x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[2000,3000,4000],[3000,4000,5000],[4000,5000,6000], [100,200,300]])
+y = np.array([4,5,6,7,8,9,10,11,12,13,5000,6000,7000, 400])
+x_pred = np.array([55,85,75])
 
 #1개짜리 데이터를 넣을떄 input_dim = 1
-x = x.reshape(x.shape[0], x.shape[1], 1)
 
-#x = x.reshape(x.shape[0], x.shape[1], 1)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+scaler = StandardScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+x_pred = scaler.transform(x_pred.reshape(1,3))
+
+
+print(x)
+print(x_pred)
+x = x.reshape(x.shape[0], x.shape[1], 1)
 
 # 4행 3열짜리 데이터를 한개씩 꺼내쓰겠다는 뜻
 
@@ -21,20 +29,16 @@ x = x.reshape(x.shape[0], x.shape[1], 1)
 
 #2. 모델구성
 model = Sequential()
-model.add(LSTM(1300, input_shape=(3,1), return_sequences=True)) 
-model.add(LSTM(1300, return_sequences=True)) 
-model.add(LSTM(1300, return_sequences=True)) 
-model.add(LSTM(1300, return_sequences=True)) 
-model.add(LSTM(1300, return_sequences=False))
+model.add(LSTM(800, input_shape=(3,1))) 
 # 데이터의 개수인 행은 무시하고 x의 shape
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
-model.add(Dense(300))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
 model.add(Dense(1))
 
 model.summary()
@@ -52,13 +56,9 @@ model.summary()
 
 #3. 실행
 model.compile(loss = 'mse', optimizer='adam')
-model.fit(x,y,epochs=2000,batch_size = 32)
+model.fit(x,y,epochs=1000)
 
 x_pred = x_pred.reshape(1,3,1)  ## 같은 크기의 행렬로 만들어줌
 
 y_pred = model.predict(x_pred)
 print(y_pred)
-
-
-## LSTM에서 나가는 값이 순차적 데이터가 아니라 제데로 적합이 안됨
-## 나가는 값이 순차적 데이터가 될 수 있도록 해줄 필요가 있다.
