34eb2fdd848ed1d11ea395a92b9f88b4eaba685e
Inerial <rlxo132@naver.com>
2020-05-22 14:22:47 +0900

    3

---
+++ keras/keras38_minmax.py
@@ -0,0 +1,64 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+# 실습 : LSTM 레이어를 5개 엮어서 dense결과를 이겨내시오
+
+#1. 데이터
+x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[20,30,40],[30,40,50],[40,50,60]])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+x_pred = np.array([55,65,75])
+
+#1개짜리 데이터를 넣을떄 input_dim = 1
+x = x.reshape(x.shape[0], x.shape[1], 1)
+
+#x = x.reshape(x.shape[0], x.shape[1], 1)
+
+# 4행 3열짜리 데이터를 한개씩 꺼내쓰겠다는 뜻
+
+
+## LSTM 함수는 기본적으로 2차원 함수로 출력되
+
+#2. 모델구성
+model = Sequential()
+model.add(LSTM(1300, input_shape=(3,1), return_sequences=True)) 
+model.add(LSTM(1300, return_sequences=True)) 
+model.add(LSTM(1300, return_sequences=True)) 
+model.add(LSTM(1300, return_sequences=True)) 
+model.add(LSTM(1300, return_sequences=False))
+# 데이터의 개수인 행은 무시하고 x의 shape
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(300))
+model.add(Dense(1))
+
+model.summary()
+## 첫 LSTM layer에 input_shape를 (3,1)을 넣었을때 return_sequences = True를 주지 않으면 리턴시키는 값의 shape가 (batch_size, time_step, feature)의 형태가 아닌
+## 기존의 (batch_size, output)의 형태로 나오게 설정되어있다.
+## 연속으로 LSTM을 쓰기 위해서는 return_sequences=True를 써주어야 한다. 반대로 Dense를 쓰기 위해서는 다시 false값을 주어야한다(디폴트값)
+
+## one layer에 one bias  다양한 bias가 필요하다면 LSTM을 더 만드는 것이 좋다.
+
+## parameter 계산은 여전히 (input_dim + output + 1) * output * 4 이다.
+## 이떄 두번째 이후 input은 이전 LSTM의 output과 같다.
+
+## return_sequences=True값을 준다면 LSTM의 shape형태로 출력되므로 output_shape = (none, 원래 준 time_step, output크기(output노드수 == 다음레이어의 input노드수))
+
+
+#3. 실행
+model.compile(loss = 'mse', optimizer='adam')
+model.fit(x,y,epochs=2000,batch_size = 32)
+
+x_pred = x_pred.reshape(1,3,1)  ## 같은 크기의 행렬로 만들어줌
+
+y_pred = model.predict(x_pred)
+print(y_pred)
+
+
+## LSTM에서 나가는 값이 순차적 데이터가 아니라 제데로 적합이 안됨
+## 나가는 값이 순차적 데이터가 될 수 있도록 해줄 필요가 있다.
