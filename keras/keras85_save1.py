34672f91bd6a0f5bd68fbd692e71e74f0d55ac3a
Inerial <rlxo132@naver.com>
2020-06-01 10:59:30 +0900

    2

--- keras/keras85_save1.py
+++ keras/keras85_save1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from keras.datasets import mnist
 
+## 1 : 데이터 전처리
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
 print(x_train[0])
@@ -22,9 +23,11 @@ y_test = np_utils.to_categorical(y_test)
 
 ## 정규화
 #from sklearn.preprocessing import MinMaxScaler
-x_train = x_train.reshape(60000,28,28,1).astype('float32') / 255 ## float 안해줘도 되지 않나?
+x_train = x_train.reshape(60000,28,28,1).astype('float32') / 255
 x_test = x_test.reshape(10000,28,28,1).astype('float32') / 255
 
+
+## 2 : 모델링
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, Flatten, MaxPool2D
 
@@ -50,7 +53,11 @@ model.add(Dense(200, activation='elu'))
 model.add(Dense(200, activation='elu'))
 model.add(Dense(10, activation='softmax'))
 
+# model.save('./model/model_test01.h5')
+## 이때는 모델만 저장됨
 
+
+###  3 : 훈련
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
 from keras.callbacks import EarlyStopping
@@ -59,7 +66,11 @@ early = EarlyStopping(monitor='loss', patience = 5, mode = 'auto')
 hist = model.fit(x_train, y_train, batch_size = 100, epochs=30, validation_split=0.1, callbacks=[early])
 
 model.save('./model/model_test01.h5')
+## save한 loss,acc와 load한 loss,acc가 똑같다.
+## fit 이후 save시 가중치까지 모델에 저장이 된다.
 
+
+## 4 : 평가
 loss, acc = model.evaluate(x_test,y_test)
 print('loss :',loss)
 print('acc :',acc)
@@ -70,7 +81,6 @@ val_loss = hist.history['val_loss']
 acc = hist.history['acc']
 val_acc = hist.history['val_acc']
 
-""" 
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10,6))
@@ -101,4 +111,7 @@ plt.xlabel('epoch')
 plt.legend(['acc', 'val_acc'])
 
 plt.show()
- """
+
+
+""" loss : 0.13090835977123425
+acc : 0.9800999760627747 """
