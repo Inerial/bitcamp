9ecf34b7db682666be8cc098e10faf03adf3bce2
Inerial <rlxo132@naver.com>
2020-06-01 17:13:39 +0900

    123

--- keras/keras95_save_pd.py
+++ keras/keras95_save_pd.py
@@ -11,10 +11,15 @@ print(datasets)
 print(datasets.head()) ## 위에서 5개만 보여주는것
 print(datasets.tail()) ## 아래에서 5개만 보여주는것
 
-# print(datasets.values) ## np.array형태로
+# print(datasets.values) ## np.array형태로 안에 가중치가 높은 데이터형태로 전부 바뀌어서 만들어짐
+## string형태가 존재시 모든 데이터가 string
+## string x , float x, int만 있다면 모든 데이터가 int (boolean도 포함)
+## 역시 boolean만 있으면 모든 데이터가 boolean으로 될 것으로 보임
 
 x = datasets.values[:, :-1]
 y = datasets.values[:, -1]
-
+print(x) 
+print(y)
+""" 
 np.save('./data/iris_x.npy', arr = x)
-np.save('./data/iris_y.npy', arr = y)
+np.save('./data/iris_y.npy', arr = y) """
