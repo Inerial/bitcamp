0b042ac73ec9a40ba8ee008659b5b7c3a664e80a
Inerial <rlxo132@naver.com>
2020-06-11 12:48:59 +0900

    3

--- dacon/comp1/dacon01_plot.py
+++ dacon/comp1/dacon01_plot.py
@@ -8,9 +8,9 @@ from sklearn.tree import DecisionTreeRegressor
 train = pd.read_csv('./data/dacon/comp1/train.csv', sep=',', index_col = 0, header = 0)
 test = pd.read_csv('./data/dacon/comp1/test.csv', sep=',', index_col = 0, header = 0)
 
-train_col = train.columns[:-4]
-test_col = test.columns
-y_train_col = train.columns[-4:]
+train_col = list(train.columns[:-4])
+test_col = list(test.columns)
+y_train_col = list(train.columns[-4:])
 y_train = train.values[:,-4:]
 train = train.values[:,:-4]
 test = test.values
@@ -33,28 +33,35 @@ train_dst = train.filter(regex='_dst$',axis=1).T.interpolate().fillna(method ='f
 test_src = test.filter(regex='_src$',axis=1).T.interpolate().fillna(method ='ffill').fillna(method ='bfill').T.values
 test_dst = test.filter(regex='_dst$',axis=1).T.interpolate().fillna(method ='ffill').fillna(method ='bfill').T.values
 
-
 x_train = np.concatenate([train.values[:,0:1], train_src, train_dst, train_src - train_dst], axis = 1)
 x_pred = np.concatenate([test.values[:,0:1], test_src, test_dst, test_src - test_dst], axis = 1)
 
+train_col = train_col + [str(i*10+650)+'_diff' for i in range(35)]
+test_col = test_col + [str(i*10+650)+'_diff' for i in range(35)]
 # print(train.filter(regex='_dst$',axis=1))
-# for i in range(5):
-#     plt.subplot(2,1,1)
-#     pd.DataFrame(x_train, columns=train_col).filter(regex='_src$',axis=1).iloc[i, :].plot()
-#     plt.subplot(2,1,2)
-#     pd.DataFrame(x_train, columns=train_col).filter(regex='_dst$',axis=1).iloc[i, :].plot()
-#     plt.show()
+for i in range(5):
+    plt.subplot(2,2,1)
+    pd.DataFrame(x_train, columns=train_col).filter(regex='_src$',axis=1).iloc[i, :].plot()
+    plt.subplot(2,2,2)
+    pd.DataFrame(x_train, columns=train_col).filter(regex='_dst$',axis=1).iloc[i, :].plot()
 
-# for i in range(5):
-#     X1 = train_src[i]
-#     X1 = np.concatenate([X1,X1,X1,X1,X1])
-#     Y1 = np.fft.fft(X1)
-#     P1 = abs(Y1/175)
-#     P1[2:-1] = 2*P1[2:-1]
-#     f = 1000*np.array(range(0,int(175)))/175
+    X1 = train_src[i]
+    X2 = train_dst[i]
+    X1 = np.concatenate([X1])
+    X2 = np.concatenate([X2])
+    Y1 = np.fft.fft(X1)
+    Y2 = np.fft.fft(X2)
+    P1 = abs(Y1/35)
+    P2 = abs(Y2/35)
+    P1[2:-1] = 2*P1[2:-1]
+    P2[2:-1] = 2*P2[2:-1]
+    f = 1000*np.array(range(0,int(35)))/35
 
-#     plt.plot(f,P1)
-#     plt.show()
+    plt.subplot(2,2,3)
+    plt.plot(f,P1)
+    plt.subplot(2,2,4)
+    plt.plot(f,P2)
+    plt.show()
 
 # train[:,1:] = train[:,1:] / train[:,0].reshape(train.shape[0],1) / train[:,0].reshape(train.shape[0],1)
 # test[:,1:] = test[:,1:] / test[:,0].reshape(test.shape[0],1) / test[:,0].reshape(test.shape[0],1)
@@ -69,39 +76,39 @@ x_pred = np.concatenate([test.values[:,0:1], test_src, test_dst, test_src - test
 
 
 
-def plot_feature_importacnes_cancer(model, title):
-    n_features = x_train.shape[1]
-    plt.barh(np.arange(n_features), model.feature_importances_, align = 'center')
-    plt.yticks(np.arange(n_features), train_col)
-    plt.title(title)
-    plt.xlabel("feature_importace")
-    plt.ylabel("Features")
-    plt.ylim(-1, n_features)
+# def plot_feature_importacnes_cancer(model, title):
+#     n_features = x_train.shape[1]
+#     plt.barh(np.arange(n_features), model.feature_importances_, align = 'center')
+#     plt.yticks(np.arange(n_features), train_col)
+#     plt.title(title)
+#     plt.xlabel("feature_importace")
+#     plt.ylabel("Features")
+#     plt.ylim(-1, n_features)
 
-model = DecisionTreeRegressor()
-model.fit(x_train,y_train)
-# print(model.feature_importances_)
-plot_feature_importacnes_cancer(model, "DecisionTree")
-plt.show()
+# model = DecisionTreeRegressor()
+# model.fit(x_train,y_train)
+# # print(model.feature_importances_)
+# plot_feature_importacnes_cancer(model, "DecisionTree")
+# plt.show()
 
-model = RandomForestRegressor()
-model.fit(x_train,y_train)
-# print(model.feature_importances_)
-plot_feature_importacnes_cancer(model, "RandomForest")
-plt.show()
+# model = RandomForestRegressor()
+# model.fit(x_train,y_train)
+# # print(model.feature_importances_)
+# plot_feature_importacnes_cancer(model, "RandomForest")
+# plt.show()
 
-for i in range(4):
-    model = GradientBoostingRegressor()
-    model.fit(x_train,y_train[:,i])
-    # print(model.feature_importances_)
-    plt.subplot(2,2,i+1)
-    plot_feature_importacnes_cancer(model, "GradientBoost" + str(i))
-plt.show()
+# for i in range(4):
+#     model = GradientBoostingRegressor()
+#     model.fit(x_train,y_train[:,i])
+#     # print(model.feature_importances_)
+#     plt.subplot(2,2,i+1)
+#     plot_feature_importacnes_cancer(model, "GradientBoost" + str(i))
+# plt.show()
 
-for i in range(4):
-    model = XGBRegressor(validate_parameters= True, n_jobs= -1, n_estimators= 1000, max_depth= 5, eta= 0.1)
-    model.fit(x_train,y_train[:,i])
-    # print(model.feature_importances_)
-    plt.subplot(2,2,i+1)
-    plot_feature_importacnes_cancer(model, "XGBoost"+str(i))
-plt.show()
+# for i in range(4):
+#     model = XGBRegressor(validate_parameters= True, n_jobs= -1, n_estimators= 1000, max_depth= 5, eta= 0.1)
+#     model.fit(x_train,y_train[:,i])
+#     # print(model.feature_importances_)
+#     plt.subplot(2,2,i+1)
+#     plot_feature_importacnes_cancer(model, "XGBoost"+str(i))
+# plt.show()
--- dacon/comp1/dacon01_xgb_ML.py
+++ dacon/comp1/dacon01_xgb_ML.py
@@ -9,28 +9,30 @@ x_train = np.load('./dacon/comp1/x_train.npy')
 y_train = np.load('./dacon/comp1/y_train.npy')
 x_pred = np.load('./dacon/comp1/x_pred.npy')
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x_train, y_train, train_size = 0.8, random_state = 66
-)
+# x_train, x_test, y_train, y_test = train_test_split(
+#     x_train, y_train, train_size = 0.8, random_state = 66
+# )
 print(x_train.shape)
 print(y_train.shape)
-print(x_test.shape)
+# print(x_test.shape)
 
 # 2. model
 
 parameters = {
-    'booster' : ['gbtree', 'gblinear', 'dart'],
+    'n_estimators' : [1,5,10,20,30,50,100,1000],
+    'eta' : [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],
+    'max_depth' :[1,2,3,5,10,100],
     'validate_parameters' : [True, False],
-    'n_jobs' : [-1]
+    'n_jobs' : [-1], 'eval_metric' : ['mae']
 }
 y_pred = []
-search = RandomizedSearchCV(XGBRegressor(), parameters, cv = 5, n_iter=5)
+search = RandomizedSearchCV(XGBRegressor(), parameters, cv = 5, n_iter=50)
 
 for i in range(4):
     search.fit(x_train, y_train[:,i])
 
     print(search.best_params_)
-    print("MAE :", search.score(x_test,y_test[:,i]))
+    # print("MAE :", search.score(x_test,y_test[:,i]))
 
     y_pred.append(search.predict(x_pred))
 
